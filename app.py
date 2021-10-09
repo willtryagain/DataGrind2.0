9b94a43cc79c8b71ce50df01986b8ae25d7a8391
willtryagain <amanatman@gmail.com>
2021-10-09 16:34:52 +0530

    bar

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 df = pd.read_csv("archive/indexData.csv", parse_dates=["Date"])
 
 available_indexes = df['Index'].unique()
-# available_indexes = [1]
+years = [str(year) for year in df['Date'].dt.year.unique()]
+years = years[0::5]
 
 app.layout = html.Div([
     html.Div([
@@ -28,10 +29,20 @@ app.layout = html.Div([
 
     ],
     style={'padding': '10px 5px'}),
+
+    html.Div(dcc.Slider(
+        id='crossfilter-year--slider',
+        min=df['Date'].dt.year.min(),
+        max=df['Date'].dt.year.max(),
+        value=df['Date'].dt.year.max(),
+        marks={yr : yr for yr in years},
+        step=1
+    ), style={'padding': '0px 20px 20px 20px'}),
+
     html.Div([
         dcc.Graph(
             id='crossfilter-indicator-scatter',
-            hoverData={'points': [{'customdata': 'Japan'}]}
+            hoverData={'points': [{'customdata': 'xyz'}]}
         )
     ], style={'display': 'inline-block', 'padding': '0 20'}),
 ])
@@ -42,7 +53,7 @@ app.layout = html.Div([
     [dash.dependencies.Input('crossfilter-xaxis-column', 'value')])
 def update_graph(xaxis_column_name):
     dff = df[df['Index'] == xaxis_column_name]
-    fig = px.scatter(x=dff['Date'], y=dff['Volume'])
+    fig = px.scatter(dff, x='Date', y="Volume")
     fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
     return fig
 
